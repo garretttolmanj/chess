27d98f876d0c568290098beb52c0656e70276db1
garretttolmanj <garretttolmanj@gmail.com>
2024-11-13 18:31:53 -0700

    Implemented a working version of the list games and create game functions in the post login ui.

--- client/src/main/java/ui/PostLoginClient.java
+++ client/src/main/java/ui/PostLoginClient.java
@@ -5,16 +5,19 @@ import requestResponse.ServerResponse;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
 
 public class PostLoginClient implements Client{
     private final ServerFacade server;
     private final Repl repl;
     private final String authToken;
+    private ArrayList<Integer> gameIDs;
 
     public PostLoginClient(String serverUrl, Repl repl, String authToken) {
         server = new ServerFacade(serverUrl);
         this.repl = repl;
         this.authToken = authToken;
+        this.gameIDs = new ArrayList<>();
     }
 
     public String eval(String input) {
@@ -25,11 +28,21 @@ public class PostLoginClient implements Client{
             return switch (cmd) {
                 case "list" -> listGames();
                 case "logout" -> logout();
+                case "create" -> createGame(params);
                 default -> help();
             };
         } catch (RuntimeException ex) {
             return ex.getMessage();
         }
+    }
+
+    public String createGame(String... params) throws RuntimeException {
+        if (params.length == 1) {
+            String gameName = params[0];
+            ServerResponse response = server.createGame(gameName, authToken);
+            return String.format("Successful: Created game called " + gameName);
+        }
+        throw new RuntimeException("Expected: create <gameName>");
     }
 
     public String listGames() throws RuntimeException {
@@ -39,26 +52,32 @@ public class PostLoginClient implements Client{
 
         int i = 0;
         while (i < gameList.size()) {
+            Integer gameID = gameList.get(i).gameID();
             String gameName = gameList.get(i).gameName();
-            games.concat("-" + i+1 + gameName + "\n");
+            String whiteUsername = (gameList.get(i).whiteUsername() != null) ? gameList.get(i).whiteUsername() : "";
+            String blackUsername = (gameList.get(i).blackUsername() != null) ? gameList.get(i).blackUsername() : "";
+            games += "- " + (i + 1) + " " + gameName + " WHITE[" + whiteUsername + "] BLACK[" + blackUsername +"]" + "\n";
+            gameIDs.add(gameID);
             i++;
         }
 
-        return String.format("Current games: \n" + games);
+        return "Current games: \n" + "- ID Name Players \n" + games;
     }
+
+
 
     public String logout() throws RuntimeException {
         server.logout(authToken);
         repl.signOut();
-        return "Successful: User signed out.";
+        return "Successful: User signed out";
     }
 
     public String help() {
         return """
                 - list
                 - create <gameName>
-                - play <gameName> <BLACK/WHITE>
-                - observe <gameName>
+                - play <ID> <BLACK/WHITE>
+                - observe <ID>
                 - logout
                 """;
     }
