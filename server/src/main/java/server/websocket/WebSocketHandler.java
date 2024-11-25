23f366469192e09a7420b3fe27f8e33221729a23
garretttolmanj <garretttolmanj@gmail.com>
2024-11-25 12:22:37 -0700

    Moved some of the WebSocketHandler code to the WebSocketService for better code practices.

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -67,56 +67,11 @@ public class WebSocketHandler {
     private void saveSession(Integer gameID, Session session) {}
 
     private void connect(Session session, String username, UserGameCommand command) throws IOException, DataAccessException {
-        try {
-            // Add user to the game
-            Integer gameID = command.getGameID();
-            GameData gameData = socketService.getGame(gameID);
-            ChessGame chessGame = gameData.game();
-            ServerMessage loadGame = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
-            loadGame.setChessGame(chessGame);
-            sendMessage(session.getRemote(), loadGame);
-            connections.add(gameID, username, session);
-
-            // Broadcast connection to other users
-            var message = String.format("%s is in the game", username);
-            var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
-            notification.setMessage(message);
-            connections.broadcast(gameID, username, notification);
-        } catch (DataAccessException ex) {
-            ServerMessage error = new ServerMessage(ServerMessage.ServerMessageType.ERROR);
-            error.setErrorMessage(ex.getMessage());
-            sendMessage(session.getRemote(), error);
-        }
-
+        socketService.connect(session, username, command, connections);
     }
 
     private void makeMove(Session session, String username, UserGameCommand command) throws DataAccessException, InvalidMoveException, IOException {
-        // get the game
-        Integer gameID = command.getGameID();
-        GameData gameData = socketService.getGame(gameID);
-        ChessGame chessGame = gameData.game();
-        ChessMove chessMove = command.getChessMove();
-        ChessPosition startPosition = chessMove.getStartPosition();
-        try {
-            // Try making the move and updating the game
-            chessGame.makeMove(chessMove);
-            socketService.updateGame(gameID, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), chessGame);
-            // broadcast load game to all clients in the game
-            ServerMessage loadGame = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
-            loadGame.setChessGame(chessGame);
-            connections.broadcast(gameID, "", loadGame);
-            // broadcast notification of move to all other clients
-            var message = String.format("%s moved their piece", username);
-            var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
-            notification.setChessMove(chessMove);
-            notification.setMessage(message);
-            connections.broadcast(gameID, username, notification);
-        } catch (InvalidMoveException ex) {
-            ServerMessage error = new ServerMessage(ServerMessage.ServerMessageType.ERROR);
-            error.setErrorMessage("Invalid Move");
-            sendMessage(session.getRemote(), error);
-        }
-
+        socketService.makeMove(session, username, command, connections);
     }
 
     private void leaveGame(Session session, String username, UserGameCommand command) {}
--- server/src/main/java/service/WebSocketService.java
+++ server/src/main/java/service/WebSocketService.java
@@ -1,8 +1,18 @@
 package service;
 
 import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+import chess.InvalidMoveException;
+import com.google.gson.Gson;
 import dataaccess.*;
 import model.GameData;
+import org.eclipse.jetty.websocket.api.Session;
+import server.websocket.ConnectionManager;
+import websocket.commands.UserGameCommand;
+import websocket.messages.ServerMessage;
+
+import java.io.IOException;
 
 public class WebSocketService extends Service {
     // Initialize Data Access Objects
@@ -21,8 +31,59 @@ public class WebSocketService extends Service {
     }
 
     public GameData getGame(Integer gameID) throws DataAccessException {
-        GameData gameData = gameAccess.getGame(gameID);
-        return gameData;
+        return gameAccess.getGame(gameID);
+    }
+
+    public void connect(Session session, String username, UserGameCommand command, ConnectionManager connections) throws IOException {
+        try {
+            // Add user to the game
+            Integer gameID = command.getGameID();
+            GameData gameData = getGame(gameID);
+            ChessGame chessGame = gameData.game();
+            connections.add(gameID, username, session);
+            // Send LOAD GAME to the user
+            ServerMessage loadGame = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
+            loadGame.setChessGame(chessGame);
+            session.getRemote().sendString(new Gson().toJson(loadGame));
+
+            // Broadcast connection to other users
+            var message = String.format("%s is in the game", username);
+            var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+            notification.setMessage(message);
+            connections.broadcast(gameID, username, notification);
+        } catch (DataAccessException | IOException ex) {
+            ServerMessage error = new ServerMessage(ServerMessage.ServerMessageType.ERROR);
+            error.setErrorMessage(ex.getMessage());
+            session.getRemote().sendString(new Gson().toJson(error));
+        }
+    }
+
+    public void makeMove(Session session, String username, UserGameCommand command, ConnectionManager connections) throws DataAccessException, IOException {
+        // get the game
+        Integer gameID = command.getGameID();
+        GameData gameData = getGame(gameID);
+        ChessGame chessGame = gameData.game();
+        ChessMove chessMove = command.getChessMove();
+        ChessPosition startPosition = chessMove.getStartPosition();
+        try {
+            // Try making the move and updating the game
+            chessGame.makeMove(chessMove);
+            updateGame(gameID, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), chessGame);
+            // broadcast load game to all clients in the game
+            ServerMessage loadGame = new ServerMessage(ServerMessage.ServerMessageType.LOAD_GAME);
+            loadGame.setChessGame(chessGame);
+            connections.broadcast(gameID, "", loadGame);
+            // broadcast notification of move to all other clients
+            var message = String.format("%s moved their piece", username);
+            var notification = new ServerMessage(ServerMessage.ServerMessageType.NOTIFICATION);
+            notification.setChessMove(chessMove);
+            notification.setMessage(message);
+            connections.broadcast(gameID, username, notification);
+        } catch (InvalidMoveException | IOException ex) {
+            ServerMessage error = new ServerMessage(ServerMessage.ServerMessageType.ERROR);
+            error.setErrorMessage("Invalid Move");
+            session.getRemote().sendString(new Gson().toJson(error));
+        }
     }
 
     public void updateGame(Integer gameID, String white, String black, String gameName, ChessGame chessGame) throws DataAccessException {
